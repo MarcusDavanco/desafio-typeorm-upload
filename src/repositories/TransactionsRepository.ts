fce2910024eab8dcf51b257022a125fe70f6e283
Marcus Davanço <md.daves@gmail.com>
2020-05-20 19:53:56 -0300

    Post and Get Routes

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,4 @@
 import { EntityRepository, Repository } from 'typeorm';
-
 import Transaction from '../models/Transaction';
 
 interface Balance {
@@ -11,7 +10,33 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const balance = transactions.reduce(
+      (total: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            total.income += transaction.value;
+            break;
+          case 'outcome':
+            total.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+
+        return total;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,50 @@
+import { getCustomRepository, getRepository } from 'typeorm';
 // import AppError from '../errors/AppError';
 
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: Category;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
     // TODO
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+
+    let newCategory;
+
+    const findCategory = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!findCategory) {
+      newCategory = categoriesRepository.create({
+        title,
+      });
+
+      categoriesRepository.save(newCategory);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: newCategory.id || findCategory.id,
+    });
+
+    return transaction;
   }
 }
 
